8f9d0e9cf7be2d04cc691d6dbd2d0c03b8b460e2
roye <roye.rozov@gmail.com>
2015-08-05 13:40:21 +0300

    added aya plasmids

--- sample_plasmid_set.py
+++ sample_plasmid_set.py
@@ -33,23 +33,49 @@ def readfq(fp): # this is a generator function
                 yield name, seq, None # yield a fasta record instead
                 break
 
+def get_good_plasmids_dict_from_file(pfile, min_len, max_len, res=None):
+    if res==None:
+        res = {}
+    fp = open(pfile, 'r')
+    for name,seq,qual in readfq(fp):
+        if min_len <= len(seq) <=max_len and \
+        not 'N' in seq:
+            res[name] = seq
+    return res
+
 ########################################################
 # Entry point
 
-plasmids_file = '/home/nasheran/rozovr/recycle_paper_data/NCBI_plasmids.fasta'
+# read in Aya's plasmid scaffolds, skip any containing N's
+# any that are less than 1000 bp
+data_dir = '/home/nasheran/rozovr/recycle_paper_data/'
+aya_plasmids_file = data_dir + 'Aya_plasmid_scaffolds_PNAS.fa'
+ncbi_plasmids_file = data_dir + 'NCBI_plasmids.fasta'
 samp_file = 'samp_plasmids_out.fasta'
+min_len = 1000
 max_len = 20000
-samp_size = 800
+samp_size = 900
 res = {}
-print plasmids_file
-fp = open(plasmids_file, 'r')
-for name,seq,qual in readfq(fp):
-	if len(seq)<max_len:
-		res[name] = seq
+res = get_good_plasmids_dict_from_file(aya_plasmids_file, min_len, max_len, res)
+print len(res)
+res = get_good_plasmids_dict_from_file(ncbi_plasmids_file, min_len, max_len, res)
+print len(res)
+# fp = open(plasmids_file, 'r')
+# for name,seq,qual in readfq(fp):
+# 	if min_len <= len(seq) <=max_len and \
+#     not 'N' in seq:
+# 		res[name] = seq
+
+# fp = open(aya_plasmids_file, 'r')
+# for name,seq,qual in readfq(fp):
+#     if min_len <= len(seq) <=max_len and \
+#     not 'N' in seq:
+#         res[name] = seq
+
 
 # fo = open(samp_file, 'w')
 sample_inds = random.sample(xrange(len(res)), samp_size)
 keys = res.keys()
 for ind in sample_inds:
-	print ">"+keys[ind]+"\n"+res[keys[ind]]+"\n"
+	print ">"+keys[ind]+"\n"+res[keys[ind]]
 	# fo.write(">"+keys[ind]+"\n"+res[keys[ind]]+"\n")
