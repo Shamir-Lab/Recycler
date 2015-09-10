6c0f5cb56973314a996ac44045b12a40203f5da8
roye <roye.rozov@gmail.com>
2015-09-10 23:36:16 +0300

    made makefile for running with mapping data

--- map_and_recycle.mk
+++ map_and_recycle.mk
@@ -1,15 +1,23 @@
 
-INPUT_DIR = /vol/scratch/rozovr/M_res/
+# INPUT_DIR = /home/nasheran/ayabrown/trimmed_reads/M_res/
+
 READS_DIR = /home/gaga/rozovr/recycle_paper_data/
+REF_CNT = 100
+INPUT_DIR = $(READS_DIR)/ref_$(REF_CNT)
+INPUT1 = before_rr.fasta
 
-
-all: fetch_scripts
+all: fetch_joins fetch_ands $(INPUT_DIR)/$(INPUT1).bwt \
+	$(INPUT_DIR)/$(INPUT1).ann $(INPUT_DIR)/$(INPUT1).amb \
+	$(INPUT_DIR)/$(INPUT1).sa $(INPUT_DIR)/$(INPUT1) \
+	$(INPUT_DIR)/reads_to_$(INPUT1).bam \
+	$(INPUT_DIR)/reads_to_$(INPUT1).joins.bam \
+	$(INPUT_DIR)/reads_to_$(INPUT1).ands.bam
 
 
 #### fetch scripts used for processing bam file ####
 # output bams used either for 2 step assembly or input
 # to recycler
-fetch_scripts: fetch_joins fetch_ands
+# fetch_scripts: fetch_joins fetch_ands
 
 fetch_joins: extract_contig_joining_pairs.c
 	gcc -o $@ $^ -L. -lbam -lz
@@ -17,9 +25,20 @@ fetch_joins: extract_contig_joining_pairs.c
 fetch_ands: extract_mate_pair_type_reads.c
 	gcc -o $@ $^ -L. -lbam -lz
 
+$(INPUT_DIR)/$(INPUT1).bwt $(INPUT_DIR)/$(INPUT1).ann $(INPUT_DIR)/$(INPUT1).amb $(INPUT_DIR)/$(INPUT1).sa: $(INPUT_DIR)/$(INPUT1)
+	~/bwa/bwa index $^
 
+$(INPUT_DIR)/reads_to_$(INPUT1).bam: $(INPUT_DIR)/$(INPUT1).bwt $(INPUT_DIR)/$(INPUT1).ann $(INPUT_DIR)/$(INPUT1).amb $(INPUT_DIR)/$(INPUT1).sa
+	~/bwa/bwa mem -t 16 $(INPUT_DIR)/$(INPUT1) \
+	$(READS_DIR)/plasmids_sim_ref_$(REF_CNT)_reads.1.fasta.gz \
+	$(READS_DIR)/plasmids_sim_ref_$(REF_CNT)_reads.2.fasta.gz | \
+	samtools view -buS - > $@
 
+$(INPUT_DIR)/reads_to_$(INPUT1).joins.bam: $(INPUT_DIR)/reads_to_$(INPUT1).bam
+	fetch_joins $^ $@
 
+$(INPUT_DIR)/reads_to_$(INPUT1).ands.bam: $(INPUT_DIR)/reads_to_$(INPUT1).bam
+	fetch_ands $^ $@
 
 
 
--- recycle.py
+++ recycle.py
@@ -84,56 +84,6 @@ def update_node_coverage_vals(path, G, comp, seqs, max_k_val=55):
     """ given a path, updates node coverage values
         assuming mean observed path coverage is used
     """
-    # path_copy = list(path)
-    # tot = get_total_path_mass(path,G)
-    # mean_cov = tot / get_total_path_length(path, seqs)
-    # path_determining_nodes = [] # nodes that can be used to est. cov.
-    # determined_mean = 0
-
-    # for nd in path_copy:
-    #     nd2 = rc_node(nd)        
-    #     if G.in_degree(nd)==1 and G.out_degree(nd)==1 and\
-    #         G.in_degree(nd2)==1 and G.out_degree(nd2)==1:
-    #             path_determining_nodes.append(nd)
-    # if len(path_determining_nodes)>0:
-    #     determined_mean = np.mean([get_cov_from_SPAdes_name(p,G) for p in path_determining_nodes])
-    #     for nd in path_copy:
-    #         nd2 = rc_node(nd)
-    #         if nd in G and nd in comp:
-    #             new_cov = get_cov_from_SPAdes_name(nd,G) - determined_mean
-    #             if new_cov <= 0: 
-    #                 G.remove_node(nd)
-    #                 comp.remove_node(nd)
-    #             else:
-    #                 G.add_node(nd, cov=new_cov)
-    #                 comp.add_node(nd, cov=new_cov)
-    #         if nd2 in G and nd2 in comp:
-    #             new_cov = get_cov_from_SPAdes_name(nd2,G) - determined_mean
-    #             if new_cov <= 0:
-    #                 G.remove_node(nd2)
-    #                 comp.remove_node(nd2)
-    #             else:
-    #                 G.add_node(nd2, cov=new_cov)
-    #                 comp.add_node(nd2, cov=new_cov)
-    # else: 
-    #     for nd in path_copy:
-    #         nd2 = rc_node(nd)
-    #         if nd in G and nd in comp:
-    #             new_cov = get_cov_from_SPAdes_name(nd,G) - mean_cov
-    #             if new_cov <= 0: 
-    #                 G.remove_node(nd)
-    #                 comp.remove_node(nd)
-    #             else:
-    #                 G.add_node(nd, cov=new_cov)
-    #                 comp.add_node(nd, cov=new_cov)
-    #         if nd2 in G and nd2 in comp:
-    #             new_cov = get_cov_from_SPAdes_name(nd2,G) - mean_cov
-    #             if new_cov <= 0:
-    #                 G.remove_node(nd2)
-    #                 comp.remove_node(nd2)
-    #             else:
-    #                 G.add_node(nd2, cov=new_cov)
-    #                 comp.add_node(nd2, cov=new_cov)
     
     path_copy = list(path)
     covs = np.array([get_cov_from_SPAdes_name(n,G) for n in path])
