9ced04d97d167236fe48cbe5d732adacc55f1185
roye <roye.rozov@gmail.com>
2015-10-13 14:00:57 +0300

    plot fixes

--- strat_plot.py
+++ strat_plot.py
@@ -30,6 +30,19 @@ class RefDataType:
 		else:
 			return "oh crap!"
 
+def filter_prob_vals(vals, cnts):
+	""" replaces cases having no instances
+		with last value observed, notes where they occur
+		for plotting
+	"""
+	prob_vals = []
+	for ind, v in enumerate(vals):
+		if v == 0 and ind != 0 and cnts[ind]==0:
+			vals[ind] = vals[ind-1]
+			prob_vals.append(ind)
+	return vals, prob_vals
+
+
 def ref_prop_plot(ref, prop, prop_ind, ranges):
 	if prop == 'steps':
 		x_vals = get_exp_x_mids(ranges[prop_ind],2)
@@ -38,11 +51,25 @@ def ref_prop_plot(ref, prop, prop_ind, ranges):
 	else:
 		x_vals = get_x_mids(ranges[prop_ind])
 	num_meas = sum([b[1] for b in ref.get_prop(prop)])
-	s=[a*1000./num_meas for a in [b[1] for b in ref.get_prop(prop)]]
-	axarr[prop_ind].scatter(x_vals, [b[2] for b in ref.get_prop(prop)], 
-		c=ref.color, s=s, marker=ref.marker, label = ref.get_prop('label'))
-	axarr[prop_ind].plot(x_vals, [b[2] for b in ref.get_prop(prop)], 
-		c=ref.color)
+	# s=[a*1000./num_meas for a in [b[1] for b in ref.get_prop(prop)]]
+	y_vals = [b[2] for b in ref.get_prop(prop)]
+	y_vals, probs = filter_prob_vals(y_vals, [b[1] for b in ref.get_prop(prop)])
+
+	for i in range(len(y_vals)):
+		if i == len(y_vals)-1 and prop_ind==3:
+			axarr[prop_ind].scatter(x_vals[i], y_vals[i], 
+			marker=ref.marker, label = ref.get_prop('label'),
+			facecolors=ref.color, edgecolors=ref.color, s=100)
+		elif i not in probs:
+			axarr[prop_ind].scatter(x_vals[i], y_vals[i], 
+			marker=ref.marker, 
+			facecolors=ref.color, edgecolors=ref.color, s=100)	
+		else:
+			axarr[prop_ind].scatter(x_vals[i], y_vals[i], 
+				marker=ref.marker, 
+				facecolors='none', edgecolors=ref.color, s=1000)
+
+	axarr[prop_ind].plot(x_vals, y_vals, c=ref.color)
 
 def get_x_mids(rng):
 	return 0.5 * ( np.array(rng[:-1]) + np.array(rng[1:]) ) 
@@ -69,7 +96,7 @@ ref_100 = RefDataType(
 	[(59, 59, 1.0), (6, 11, 0.55), (3, 10, 0.3), (3, 4, 0.75), (1, 2, 0.5)],
 	[(4, 4, 1.0), (39, 49, 0.8), (22, 26, 0.85), (7, 7, 1.0), (0, 0, 0)],
 	[(67, 73, 0.92), (4, 5, 0.8), (1, 4, 0.25), (0, 3, 0.0), (0, 1, 0.0)], 
-	'^', 'y', 'ref_100'
+	'^', 'y', '100'
 )
 
 ref_200 = RefDataType(
@@ -77,7 +104,7 @@ ref_200 = RefDataType(
 	[(82, 83, 0.99), (3, 12, 0.25), (12, 24, 0.5), (7, 16, 0.44), (3, 7, 0.43)],
 	[(6, 8, 0.75), (55, 71, 0.77), (37, 52, 0.71), (8, 10, 0.8), (1, 1, 1.0)],
 	[(90, 95, 0.95), (7, 17, 0.41), (4, 11, 0.36), (3, 7, 0.43), (3, 11, 0.27)],
-	'v', 'g', 'ref_200'
+	'v', 'g', '200'
 )
 
 ref_400 = RefDataType(
@@ -85,7 +112,7 @@ ref_400 = RefDataType(
 	[(146, 147, 0.99), (24, 35, 0.69), (17, 39, 0.44), (11, 19, 0.58), (7, 13, 0.54)],
 	[(17, 22, 0.77), (105, 135, 0.78), (67, 77, 0.87), (11, 14, 0.79), (5, 6, 0.83)],
 	[(174, 188, 0.93), (13, 23, 0.57), (7, 14, 0.5), (8, 14, 0.57), (3, 15, 0.2)],
-	'h', 'c', 'ref_400'
+	'h', 'c', '400'
 )
 
 ref_800 = RefDataType(
@@ -93,7 +120,7 @@ ref_800 = RefDataType(
 	[(271, 278, 0.97), (39, 83, 0.47), (30, 58, 0.52), (10, 24, 0.42), (13, 27, 0.48)],
 	[(30, 46, 0.65), (174, 230, 0.76), (127, 162, 0.78), (21, 26, 0.81), (6, 6, 1.0)],
 	[(310, 345, 0.9), (25, 53, 0.47), (12, 27, 0.44), (7, 23, 0.3), (11, 27, 0.41)],
-	's', 'r', 'ref_800'
+	's', 'r', '800'
 )
 
 ref_1600 = RefDataType(
@@ -101,7 +128,7 @@ ref_1600 = RefDataType(
 	[(432, 442, 0.98), (72, 130, 0.55), (48, 95, 0.51), (29, 58, 0.5), (19, 40, 0.47)],
 	[(70, 104, 0.67), (253, 328, 0.77), (134, 173, 0.77), (119, 137, 0.87), (20, 23, 0.87)],
 	[(500, 548, 0.91), (46, 71, 0.65), (25, 50, 0.5), (23, 62, 0.37), (12, 48, 0.25)], 
-	'o', 'b', 'ref_1600'
+	'o', 'b', '1600'
 )
 
 
@@ -114,8 +141,14 @@ for ind, prop in enumerate(props):
 	for ref in [ref_100, ref_200, ref_400, ref_800, ref_1600]:
 		# print ref, prop, ind
 		ref_prop_plot(ref, prop, ind, ranges)
-	if prop in ['steps', 'coverage']:
-		axarr[ind].set_xscale('log')
+
+	if prop == 'cv':
+		axarr[ind].set_xlim(left=0, right=0.25)
+	if prop == 'steps':
+		axarr[ind].set_xscale('log', basex=2)
+		axarr[ind].set_xlim(1,32)
+	if prop == 'coverage':
+		axarr[ind].set_xscale('log', basex=10)
 	axarr[ind].set_title(prop.upper())
 
 
